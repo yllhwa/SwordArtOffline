aaa3d65b66e6e909d862ee498e2d8493d0505fcc
yllhwa <yll2002mail@gmail.com>
2022-09-14 09:55:57 +0800

    前端加入统计多文件夹操作

--- frontend/frontend/src/utils.js
+++ frontend/frontend/src/utils.js
@@ -1,20 +1,45 @@
+let folderCount = {};
 let getTagByData = (data) => {
   let tag = {
     type: "正常",
     message: "",
     en: "success",
   };
-  if (
-    data.funcName == "CreateFileA" &&
-    (data.params[0][1].endsWith(".exe") ||
-      data.params[0][1].endsWith(".dll") ||
-      data.params[0][1].endsWith(".ocx")) &&
-    data.params[1][1] == "1073741824"
-    // hex: 0x4000000
-  ) {
-    tag.type = "危险";
-    tag.message = "写入可执行文件";
-    tag.en = "error";
+  if (data.funcName == "CreateFileA") {
+    if (
+      (data.params[0][1].endsWith(".exe") ||
+        data.params[0][1].endsWith(".dll") ||
+        data.params[0][1].endsWith(".ocx")) &&
+      data.params[1][1] == "1073741824"
+      // hex: 0x4000000
+    ) {
+      tag.type = "危险";
+      tag.message = "写入可执行文件";
+      tag.en = "error";
+    } else {
+      // 获取文件夹,可能为\\分割也可能为/
+      let folder = data.params[0][1].split("\\");
+      if (folder.length == 1) {
+        folder = data.params[0][1].split("/");
+      }
+      folder.pop();
+      if (folder.length == 0) {
+        return tag;
+      }
+
+      folder = folder.join("\\");
+      if (folderCount[folder] == undefined) {
+        folderCount[folder] = 1;
+      } else {
+        folderCount[folder]++;
+      }
+      // 若folderCount总的内容数>=3，则认为是恶意
+      if (Object.keys(folderCount).length >= 3) {
+        tag.type = "危险";
+        tag.message = "写入多个文件夹";
+        tag.en = "error";
+      }
+    }
   } else if (data.funcName == "HeapFree") {
     tag.type = "危险";
     tag.message = "异常内存释放";
@@ -22,7 +47,7 @@ let getTagByData = (data) => {
   } else if (
     data.funcName == "RegCreateKeyEx" &&
     (data.params[1][1] ==
-      "Software\\Microsoft\\Windows\\ CurrentVersion\\RunServicesOnce" ||
+      "Software\\Microsoft\\Windows\\CurrentVersion\\RunServicesOnce" ||
       data.params[1][1] ==
         "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\RunServicesOnce" ||
       data.params[1][1] ==
