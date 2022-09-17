4984f44a82c962ee2113230b5415c1012a58e1ec
yllhwa <yll2002mail@gmail.com>
2022-09-17 10:54:04 +0800

    修改示例Lua脚本

--- frontend/LuaEngine.lua
+++ frontend/LuaEngine.lua
@@ -1,17 +1,13 @@
-function getFuncParamsPointer(paramPointer, n)
-    return paramPointer + (n - 1) * 4
-end
-
 function EnterMessageBoxA(hWnd_p, lpText_p, lpCaption_p, uType_p)
     print("EnterMessageBoxA")
     print(luaGetCStringByPointer(luaGetPointerByPointer(lpText_p)))
     print(luaGetCStringByPointer(luaGetPointerByPointer(lpCaption_p)))
     local newCaption = "hacked"
-    local newText = "hacked by lua!"
+    local newText = "hacked by lua!!!"
     local newCaptionPointer = luaNewCString(newCaption)
     local newTextPointer = luaNewCString(newText)
-    luaSetPointerByPointer(lpText_p, newCaptionPointer)
-    luaSetPointerByPointer(lpCaption_p, newTextPointer)
+    luaSetPointerByPointer(lpText_p, newTextPointer)
+    luaSetPointerByPointer(lpCaption_p, newCaptionPointer)
 end
 
 function LeaveMessageBoxA(resultPointer)
